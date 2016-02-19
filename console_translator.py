f3399bfe5337235ef036d490669d671087f3e4da
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-02-19 08:37:05 -0500

    Minor change

--- console_translator.py
+++ console_translator.py
@@ -58,6 +58,8 @@ def main():
             result_str = "Result: %s\n" % (" ".join(result))
             writeFile(test_file, query_str, "a")
             writeFile(test_file, result_str, "a")
+            writeFile(test_file, graph_str, "a")
+            writeFile(test_file, graph_str_simple, "a")
         writeFile(test_file, "\n", "a")
 
     """
