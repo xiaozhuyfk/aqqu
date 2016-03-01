a062c77230d9399db5f67b49bc605fe34f557888
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-02-29 19:15:45 -0500

    Minor change

--- query_translator/util.py
+++ query_translator/util.py
@@ -253,7 +253,7 @@ wq_test = [
     "what did anton van leeuwenhoek contribute to our knowledge of cells?"
 ]
 
-test_file = "test.log"
+test_file = "wq_error.log"
 
 rank_error = [
     "who is the present newscaster on cbs evening news",
