a8c6669674d848c84b9c6cc2748599ef01330069
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-03-15 13:40:56 -0400

    Minor change

--- entity_linker/entity_linker.py
+++ entity_linker/entity_linker.py
@@ -290,14 +290,15 @@ class EntityLinker:
 
             head = []
             tail = []
-            for token in tokens:
+            token_list = text.split(" ")
+            for token in token_list:
                 if tail:
                     tail.append(tail[-1] + len(token) + 1)
                 else:
                     tail.append(len(token))
 
             for i in xrange(len(tail)):
-                head.append(tail[i] - len(tokens[i]))
+                head.append(tail[i] - len(token_list[i]))
 
             token_start = self.findPrev(head, start)
             token_end = self.findPrev(head, start) + text[start:end].count(" ") + 1
