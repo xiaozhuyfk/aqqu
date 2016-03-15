6b1f119c2b6069a89cfb9c22c28ba1dab5f7ec9d
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-03-15 16:46:56 -0400

    minor

--- query_translator/pattern_matcher.py
+++ query_translator/pattern_matcher.py
@@ -825,7 +825,6 @@ class QueryCandidateExtender:
                     if domain in self.filter_domains:
                         domain_rels.append(r)
             return domain_rels
-        print relations
         return relations
 
     def extend_entity_with_target_relation(self, query_candidate):
