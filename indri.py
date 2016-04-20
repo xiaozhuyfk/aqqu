c52b3e9cbf55c46f808f893160592f4830c0de43
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-04-19 22:48:18 -0400

    kl

--- indri.py
+++ indri.py
@@ -147,6 +147,9 @@ def fetch_document_bow(internal, e1, e2):
         for term in s:
             bow_short[term] += 1
 
+    print bow_long
+    print bow_short
+
     return (bow_long, bow_short)
 
 def fetch_documents(query_file):
@@ -271,5 +274,6 @@ if __name__ == "__main__":
     #fetch_relation_bow("discovery_technique")
     #print fetch_documents("testresult/query/discovery_technique.log")
     #main(sys.argv[1:])
-    fetch_relation_bow("astronomy_astronomical_discovery_discovery_technique")
+    #fetch_relation_bow("astronomy_astronomical_discovery_discovery_technique")
     #print kstem("imaging")
+    fetch_document_bow("51953232", "HD 209458 b", "Transit")
